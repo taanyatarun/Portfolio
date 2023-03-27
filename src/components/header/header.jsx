935988d7365c883015a5776442f7c870a5610385
TaanyaTarun <taanyatarun@gmail.com>
2023-03-27 23:11:33 +0530

    Added typed animations

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Typed from 'react-typed';
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me1.png'
@@ -9,19 +10,29 @@ const Header = () => {
         <header>
             <div className="container header__container">
                 <h5>Hello I'm</h5>
-                <h1>Taanya Tarun</h1>
+                <h1>
+                    <Typed
+                        strings={['Taanya Tarun', 'A Coder', 'A Developer', 'A Problem Solver']}
+                        typeSpeed={100}
+                        backSpeed={80}
+                        backDelay={1500}
+                        loop
+                    />
+                </h1>
+                {/* <h1 className="multitext">Taanya Tarun</h1> */}
                 <h5 className="text-light">B.Tech CS 2nd Year Student</h5>
                 <CTA />
-                <HeaderSocials />
+                <div className="header__elements">
+                    <HeaderSocials />
             
-                <div className="me">
-                    <img src={ME} alt="me" />
-                </div>
+                    <div className="me">
+                        <img src={ME} alt="me" />
+                    </div>
 
-                <a href="#contact" className="scroll__down">Scroll Down</a>
+                    <a href="#contact" className="scroll__down">Scroll Down</a>
+                </div>
             </div>
         </header>
-        
     )
 }
 
