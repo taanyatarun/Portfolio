484f8a756e67db958ec532e08b88c296c5acf117
Taanya Tarun <91074972+taanyatarun@users.noreply.github.com>
2024-04-22 22:08:59 +0530

    Update contact.jsx

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -22,33 +22,30 @@ const Contact = () => {
     return (
         <div className="contact">
             <section id="contact">
-                <div>
+               <div className="container contact__container">
                     <h5>Get In Touch</h5>
                     <h2>Contact Me</h2>
-        
-                    <div className="container contact__container">
-                        <div className="contact__options">
-                            <article className="contact__option">
-                                <HiOutlineMail className="contact__option-icons"/>
-                                <h4>Email</h4>
-                                <h5>taanyatarun@gmail.com</h5>
-                                <a href="mailto:taanyatarun@gmail.com" target="_top">Send a message</a>
-                            </article>
-                            <article className="contact__option">
-                                <SiWhatsapp className="contact__option-icons"/>
-                                <h4>Whatsapp</h4>
-                                <h5>+918463807159</h5>
-                                <a href="https://api.whatsapp.com/send?phone=+918463807159" target="_blank">Send a message</a>
-                            </article>
-                        </div>
-        
-                        <form ref={form} onSubmit={sendEmail}>
-                            <input type="text" name="name" placeholder="Your Full Name" required />
-                            <input type="email" name="email" placeholder="Your Email" required />
-                            <textarea name="message" columns = "21" rows="7" placeholder="Your Message"></textarea>
-                            <button type="submit" className="btn btn-primary" onclick="this.form.reset();">Send Message</button>
-                        </form>
+                    <div className="contact__options">
+                        <article className="contact__option">
+                            <HiOutlineMail className="contact__option-icons"/>
+                            <h4>Email</h4>
+                            <h5>taanyatarun@gmail.com</h5>
+                            <a href="mailto:taanyatarun@gmail.com" target="_top">Send a message</a>
+                        </article>
+                        <article className="contact__option">
+                            <SiWhatsapp className="contact__option-icons"/>
+                            <h4>Whatsapp</h4>
+                            <h5>+918463807159</h5>
+                            <a href="https://api.whatsapp.com/send?phone=+918463807159" target="_blank">Send a message</a>
+                        </article>
                     </div>
+    
+                    <form ref={form} onSubmit={sendEmail}>
+                        <input type="text" name="name" placeholder="Your Full Name" required />
+                        <input type="email" name="email" placeholder="Your Email" required />
+                        <textarea name="message" columns = "21" rows="7" placeholder="Your Message"></textarea>
+                        <button type="submit" className="btn btn-primary" onclick="this.form.reset();">Send Message</button>
+                    </form>
                 </div>
             </section>
         </div>
